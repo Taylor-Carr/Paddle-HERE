6bb1758c95fe26187b42a526e8c63fd9aa30e9fc
Taylor carr <taylormichaelcarr@yahoo.com>
2024-03-12 15:29:38 +0000

    added secret key

--- paddlehere/settings.py
+++ paddlehere/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mq=%=wko*2pp^ps5$eo$u6098)k-_d^ij$5-m1aq1i^nxu%ivc'
+SECRET_KEY = 'mangos12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
