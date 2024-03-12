444e0adc4139054466df08018852d4f84390972d
Taylor carr <taylormichaelcarr@yahoo.com>
2024-03-12 17:25:24 +0000

    created home.html, associated urls and view

--- paddlehere/settings.py
+++ paddlehere/settings.py
@@ -24,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'mangos12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-taylorcarr-paddlehere-v52dqduycsq.ws-eu108.gitpod.io', '.herokuapp.com']
 
@@ -88,6 +90,11 @@ WSGI_APPLICATION = 'paddlehere.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
