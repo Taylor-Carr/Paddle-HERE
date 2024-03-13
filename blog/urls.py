8d15ec1edfea02baf0072e41960bc0322da2a607
Taylor carr <taylormichaelcarr@yahoo.com>
2024-03-13 01:21:34 +0000

    added add post html, url, view, nav link and updated blog model to get absolute url

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, BlogDetailView
+from .views import HomeView, BlogDetailView, AddPostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('blog/<int:pk>', BlogDetailView.as_view(), name="blog-details"),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
 
 ]
