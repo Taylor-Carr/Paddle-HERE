12053f5a40ad321b263a7f00a9b33d682e1f356e
Taylor carr <taylormichaelcarr@yahoo.com>
2024-03-14 21:32:20 +0000

    added comment model

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import UserRegisterView
-from django.conrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name ='register'),
-    path('password/', auth_views.PasswordChangeView.as_view()),
+    #path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
 ]
