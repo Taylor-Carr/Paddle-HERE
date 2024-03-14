739ee722d5e5de984340ca32b9223d0acff55d30
Taylor carr <taylormichaelcarr@yahoo.com>
2024-03-14 21:59:19 +0000

    added comment section to blog details html

--- blog/models.py
+++ blog/models.py
@@ -29,4 +29,4 @@ class Comment(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return '% - %' % (self.post.title, self.name)
+        return '%s - %s' % (self.post.title, self.name)
