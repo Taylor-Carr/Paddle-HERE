1cb9a10fff43fd90a31f1ae729bffa194a34a29e
Taylor carr <taylormichaelcarr@yahoo.com>
2024-03-14 00:15:21 +0000

    added landing page view for improved ux

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, BlogDetailView, AddPostView, UpdatePostView, DeletePostView
+from .views import LandingPageView, HomeView, BlogDetailView, AddPostView, UpdatePostView, DeletePostView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home"),
+    path('', LandingPageView.as_view(), name="landing_page"),
+    path('blog/', HomeView.as_view(), name="home"),
     path('blog/<int:pk>', BlogDetailView.as_view(), name="blog-details"),
     path('add_post/', AddPostView.as_view(), name="add_post"),
     path('blog/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
