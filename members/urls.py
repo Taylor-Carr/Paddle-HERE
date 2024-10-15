0c6745e1a379a8ad1e36f55e7876647aa3f25f0b
Taylor carr <taylormichaelcarr@yahoo.com>
2024-10-15 20:08:35 +0000

    updated edit profile view to handle user profile updates, created edit profile template, added url pattern efor dit  profile page

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, profile_view, edit_profile
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name ='register'),
+    path('profile/<str:username>/', profile_view, name ='profile'),
+    path('profile/<str:username>/edit/', edit_profile, name ='edit_profile'),
 ]
--- members/views.py
+++ members/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm, UserProfileForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import UserProfile
 
 
 class UserRegisterView(generic.CreateView):
@@ -13,13 +15,28 @@ class UserRegisterView(generic.CreateView):
 
     
 @login_required
-def edit_profile(request):
+def edit_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    profile = user.userprofile
+
     if request.method == 'POST':
-        form = UserProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             form.save()
-            return redirect('profile')
+            return redirect('profile', username=username)
+
     else:
+
         form = UserProfileForm(instance=request.user.userprofile)
-    return render(request, 'profile/edit_profile.html', {'form': form})
+
+    return render(request, 'edit_profile.html', {'form': form, 'profile': profile})
+
+def profile_view(request, username):
+    user =get_object_or_404(User, username=username)
+    profile = get_object_or_404(UserProfile, user=user)
+    context = {
+        'user': user,
+        'profile': profile,
+    }
+    return render (request, 'profile.html', context)
             
